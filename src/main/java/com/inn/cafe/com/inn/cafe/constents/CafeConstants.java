29b881030f78159b1ca0ebb6d38413e2484cd17b
Papa Abdou Karim WADE <abdou.karim.info@gmail.com>
2023-07-03 23:41:55 +0000

    Ajout des mots cles comme constantes dans le projet

---
+++ src/main/java/com/inn/cafe/com/inn/cafe/constents/CafeConstants.java
@@ -0,0 +1,7 @@
+package com.inn.cafe.com.inn.cafe.constents;
+
+public class CafeConstants {
+    //Ce sont les mots clés constant qu'on va utiliser dans notre projet
+    public static final String SOMETHING_WENT_WRONG= "Something went wrong";
+    public static final String INVALID_DATA= "Invalid Data";
+}
