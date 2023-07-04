1b4777a0debd5e1115cc7096baf3dc869762e0b3
Papa Abdou Karim WADE <abdou.karim.info@gmail.com>
2023-07-04 00:06:10 +0000

    Mise en place de l'implementation de UserRest et ses services

--- src/main/java/com/inn/cafe/com/inn/cafe/restImpl/UserRestImpl.java
+++ src/main/java/com/inn/cafe/com/inn/cafe/restImpl/UserRestImpl.java
@@ -1,8 +1,11 @@
 package com.inn.cafe.com.inn.cafe.restImpl;
 
+import com.inn.cafe.com.inn.cafe.constents.CafeConstants;
 import com.inn.cafe.com.inn.cafe.rest.UserRest;
 import com.inn.cafe.com.inn.cafe.service.UserService;
+import com.inn.cafe.com.inn.cafe.utils.CafeUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +23,6 @@ public class UserRestImpl implements UserRest {
         {
             ex.printStackTrace();
         }
-        return null;
+        return CafeUtils.getResponseEntity(CafeConstants.SOMETHING_WENT_WRONG,HttpStatus.INTERNAL_SERVER_ERROR)
     }
 }
--- src/main/java/com/inn/cafe/com/inn/cafe/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/inn/cafe/com/inn/cafe/serviceImpl/UserServiceImpl.java
@@ -1,9 +1,35 @@
 package com.inn.cafe.com.inn.cafe.serviceImpl;
 
+import com.inn.cafe.com.inn.cafe.constents.CafeConstants;
 import com.inn.cafe.com.inn.cafe.service.UserService;
+import com.inn.cafe.com.inn.cafe.utils.CafeUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
 
+@Slf4j
 @Service
 public class UserServiceImpl implements UserService {
+    @Override
+    public ResponseEntity<String> signUp(Map<String, String> requestMap) {
+        log.info("Inside signup {}",requestMap);
+        if (validateSignUpMap(requestMap)){
+
+        }
+        else {
+            return CafeUtils.getResponseEntity(CafeConstants.INVALID_DATA, HttpStatus.BAD_REQUEST);
+        }
+
+    }
+    private Boolean validateSignUpMap(Map<String,String> requestMap){
+        if (requestMap.containsKey("name") && requestMap.containsKey("contactNumber")
+                && requestMap.containsKey("email") && requestMap.containsKey("password"))
+        {
+            return true;
+        }
+        return false;
+    }
 }
